8c0f56721c47f3381524cd735a46ef7a74bf1b99
Vangelis Katikaridis <drakos74@gmail.com>
2020-12-11 17:20:48 +0100

    fix the build
    Signed-off-by: Vangelis Katikaridis <drakos74@gmail.com>

--- xmachina/net/ff/neuron.go
+++ xmachina/net/ff/neuron.go
@@ -10,12 +10,12 @@ import (
 
 type memory struct {
 	input  xmath.Vector
-	output float64 //nolint:golint,unused
+	output float64 //nolint
 }
 
 type learn struct {
 	weights xmath.Vector
-	bias    float64 //nolint:golint,unused
+	bias    float64 //nolint
 }
 
 type Neuron struct {
